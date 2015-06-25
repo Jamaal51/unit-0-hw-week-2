e581e4199a879c51dad8b8ec4df0fd471b57a26e
Jamaal <jsedayao@gmail.com>
2015-06-25 15:02:12 -0400

    second commit

--- CaesarCipher/CaesarCipher/main.m
+++ CaesarCipher/CaesarCipher/main.m
@@ -6,12 +6,29 @@
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
+//2. Cryptography : Caesar Cipher
+//
+//The Roman General Julius Caesar used to correspond with his generals using a secret code. He devised a way of encrypting his messages using a simple encryption scheme now known as Caesar Cipher or Shift Cipher. You can read more about it here and watch a video here
+//
+//You are given a class called CaesarCipher with methods encode and decode
+//Being amateur codebreakers, we want to know if two distinct looking ciphers correspond to the same input message. Write a method called codeBreaker, which accepts two cipher strings as paramaters and returns a boolean value which tells us whether they are actually the same input message encoded using two different offsets. hint: the maximum offset is 25
+//There are multiple ways to do this. Try to come up with as many solutions as you can.
+//Example:
+//okmg = "mike", offset 2
+//tprl = "mike", offset 7
+//
+//Both are the same input message, but different offset. Your method would return YES in this case
+
 #import <Foundation/Foundation.h>
+
 
 @interface CaesarCipher : NSObject
 
 - (NSString *)encode:(NSString *)string offset:(int)offset;
 - (NSString *)decode:(NSString *)string offset:(int)offset;
+- (BOOL) codeBreaker:(NSString *)stringOne                  //declare method of codebreaker
+                with:(NSString *)stringTwo;
+- (void) printself;
 
 @end
 
@@ -50,11 +67,45 @@
     return [self encode:string offset: (26 - offset)];
 }
 
+- (BOOL) codeBreaker:(NSString *)stringOne                              //method of codebreaker
+                with:(NSString *)stringTwo {
+    
+    BOOL isSameMessage = NO;
+    
+    for (int i = 1; i < 26; i++){
+        NSString *decodedStringOne = [self decode:stringOne offset:i];    //nested for-loop each index i it goes through 25 offsets of j
+        for (int j = 1; j < 26; j++){
+            NSString *decodedStringTwo = [self decode:stringTwo offset:j];
+            
+            if ([decodedStringOne isEqualToString:decodedStringTwo]) {    //conditional statement checks if any decode of stringOne matches stringTwo then YES, same base word
+                isSameMessage = YES;
+            }
+        }
+    }
+        return isSameMessage;
+
+}
+
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        
+        CaesarCipher *firstCode = [[CaesarCipher alloc] init];
+        
+        [firstCode encode:@"king" offset:3];
+        
+        NSLog(@"%@", [firstCode encode:@"king" offset:3]);
+    
+        BOOL isSameMessage = [firstCode codeBreaker:@"ljoh" with:@"yrpi"];
+        NSLog(@"%d", isSameMessage);
+        
+        
+//        CaesarCipher *secondCode = [[CaesarCipher alloc] init];
+     
+        
+
     }
 }
--- Person/Person/main.m
+++ Person/Person/main.m
@@ -6,58 +6,151 @@
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
+//1. Objective-C Classes
+//
+//You are provided with a Person class. This class has private properties name, phoneNumber and city, along with their getter and setter methods.
+//
+//Write a method called checkSameCity which accepts one parameter of type Person * and checks if they live in the same city. The method should return a boolean value.
+//A Person has recently had a child, whose name is 'Abc'. Write a method called registerChild which takes 0 parameters and returns a new Person * instance represeting the child, which has the same phoneNumber and city as the parent.
+
+
 #import <Foundation/Foundation.h>
 
 @interface Person: NSObject
 
-- (void)setName:(NSString *)name;
+- (void)setName:(NSString *)name; // setName is method
 - (NSString *)name;
 
-- (void)setCity:(NSString *)city;
+- (void)setCity:(NSString *)city;   //making a method = passing it NSString parameter to              set city property
 - (NSString *)city;
 
 - (void)setPhoneNumber:(NSString *)phoneNumber;
 - (NSString *)phoneNumber;
 
+- (void)changePersonName:(Person *)aPerson       //multiple parameters
+                  toName:(NSString *)newName;   // press enter lines up colons
+
+- (BOOL) checkSameCity:(Person *)aPerson;
+
+//- (BOOL)checkSameCity_isEqualToString:(NSObject *)Person;
+
+- (Person *)haveChild;
+
 @end
 
 @implementation Person {
     NSString *_name;
-    NSString *_phoneNumber;
+    NSString *_phoneNumber;       //instance variables
     NSString *_city;
+    BOOL *_sameCity;
 }
 
-- (void)setName:(NSString *)name {
+- (void)setName:(NSString *)name {    //same as interface above
     _name = name;
 }
 
-- (NSString *)name {
+- (NSString *)name {                  //same as interface above
     return _name;
 }
 
 - (void)setCity:(NSString *)city {
-    _city = city;
+    _city = city;                    // _city accesses instance property. set to value of parameter
 }
 
-- (NSString *)city {
-    return _city;
+- (NSString *)city {                 //type of NSString, name of method is city
+    return _city;                    //return value of instance variable
 }
 
-- (void)setPhoneNumber:(NSString *)phoneNumber {
+- (void)setPhoneNumber:(NSString *)phoneNumber {       // setter
     _phoneNumber = phoneNumber;
 }
 
-- (NSString *)phoneNumber {
+- (NSString *)phoneNumber {                             //getter
     return _phoneNumber;
 }
+
+
+-(void)changePersonName:(Person *)aPerson
+                 toName:(NSString *)newName{
+    
+    [aPerson setName:newName];
+}
+
+
+- (BOOL) checkSameCity:(Person *)aPerson{
+    if ([[aPerson city] isEqualToString:[self city]]) { // self refers to this object running ie Mike checkSameCity: Carl ... self is mike
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+- (Person *)haveChild{
+    Person* child = [[Person alloc] init];
+    [child setCity: [self city]];
+    [child setPhoneNumber: [self phoneNumber]];
+    
+    return child;
+}
+
+
+//- (BOOL)checkSameCity_isEqualToString: (NSObject *)Person {
+//       return *(_sameCity);
+
+//- (BOOL)isEqualToString:(NSString *)aString
+
 
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        Person *first = [[Person alloc] init];
+        
+        [first setName:@"Victoria"];              // [ object   message to object];
+        [first setCity:@"Maui"];
+        [first setPhoneNumber:@"777-777-7777"];
+        NSLog(@"My friend's name is %@. She lives in %@. Her phone number is %@.", first.name, first.city, first.phoneNumber);
+        
+        Person *second = [[Person alloc] init];
+        
+        [second setName:@"Olivia"];
+        [second setCity:@"Maui"];
+        [second setPhoneNumber:@"999-999-9999"];
+        NSLog(@"My niece's name is %@. She lives in %@. Her phone number is %@.", second.name, second.city, second.phoneNumber);
+        //    checkSameCity = [first.city, second.city];
+        
+        
+        
+        NSString *VictoriasName = [first name];   //asking Victoria (first) to give name
+        NSLog(@"%@", VictoriasName);
+        
+        [first setName:@"Vicky"];
+        NSLog(@"%@", [first name]);             //put in [ ]
+        
+        [first setCity:@"New York City"];
+        NSLog(@"%@", [first city]);
+        
+        Person *Mike = [[Person alloc] init];
+        
+        [Mike changePersonName: first toName:@"Victoria"];
+        NSLog(@"%@", [first name]);
+        
+        [Mike setCity:@"New York City"];
+        
+        BOOL citiesAreSame = [Mike checkSameCity: first];
+        NSLog(@"%d", citiesAreSame);
+        
+        Person *MikesBaby = [Mike haveChild];
+        NSLog(@"%@", [MikesBaby city]);
+        [Mike changePersonName:MikesBaby toName:@"ABC"];
+        NSLog (@"%@", [MikesBaby name]);
+        
+        
+        
+        
     }
     return 0;
 }
+
